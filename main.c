53599996320081c6b116e1acf227bcc90ee11231
Jean-BaptisteCOGNET <58042575+Jean-BaptisteCOGNET@users.noreply.github.com>
2021-11-15 19:11:32 +0100

    hall sensor

--- main.c
+++ main.c
@@ -3,14 +3,27 @@
 
 #define TIMER 1000
 
-int main() {
+int main()
+{
     // Enable the pins
     DDRD |= _BV(PD6);
+    // DDRD |= _BV(PD2);
 
-    while(1){
-        PORTD |= _BV(PD6);
-        _delay_ms(TIMER);
-        PORTD &= ~_BV(PD6);
-        _delay_ms(TIMER);
+    while (1)
+    {
+        // PORTD |= _BV(PD6);
+        // _delay_ms(TIMER);
+        // PORTD &= ~_BV(PD6);
+        // _delay_ms(TIMER);
+        if (PIND & _BV(2))
+        {
+            // PORTD |= _BV(PD6);
+            PORTD &= ~_BV(PD6);
+        }
+        else
+        {
+            PORTD |= _BV(PD6);
+            // PORTD &= ~_BV(PD6);
+        }
     }
 }
