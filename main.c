fc7005f3a122f584e3e4f5824e2df6eb27aeb997
DUCLUSAUD Marc <marc.duclusaud@bordeaux-inp.fr>
2021-11-22 14:38:39 +0100

    séance du 15

--- main.c
+++ main.c
@@ -3,14 +3,26 @@
 
 #define TIMER 1000
 
-int main() {
-    // Enable the pins
-    DDRD |= _BV(PD6);
+void init_pins() {
+    // Init all used pins
+    DDRD |= _BV(PD6); // Blue LED as output
+    DDRD &= ~_BV(PD2); // Hall sensor as input
+}
 
-    while(1){
+void blue_led_blink(int duration) {
+    // Make the blue LED blink each second
+    // duration are in seconds
+    int elapsed_time = 0;
+    while (elapsed_time < duration){
         PORTD |= _BV(PD6);
         _delay_ms(TIMER);
         PORTD &= ~_BV(PD6);
         _delay_ms(TIMER);
+        elapsed_time += 2;
     }
+}
+
+int main() {
+    init_pins();
+    blue_led_blink(120);
 }
